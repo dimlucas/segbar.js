18db397784111384872bf21e3bf77a15f077134d
Dimitris Loukas <dimlucas@outlook.com>
2017-08-18 23:37:25 +0300

    Solve issue #2

--- segbar.js
+++ segbar.js
@@ -2,7 +2,26 @@
     const DEFAULT_WIDTH = "100%";
     const DEFAULT_HEIGHT = "80px";
     const palette = [
-
+        '#26C281',
+        '#E9D460',
+        '#81CFE0',
+        '#F22613',
+        '#3A539B',
+        '#E67E22',
+        '#22A7F0',
+        '#C0392B',
+        '#663399',
+        '#913D88',
+        '#F9BF3B',
+        '#6C7A89',
+        '#DB0A5B',
+        '#03A678',
+        '#FDE3A7',
+        '#6BB9F0',
+        '#8E44AD',
+        '#D24D57',
+        '#CF000F',
+        '#86E2D5'
     ];
 
     $.fn.segbar = function(options = []) {
@@ -28,6 +47,7 @@
         element.style.width = options.width ? options.width : DEFAULT_WIDTH;
         element.style.height = options.height ? options.height : DEFAULT_HEIGHT;
         element.classList.add('segbar');
+        let colorIt = getNextColor();
 
         for(let item of options.data) {
             let div = document.createElement('div');
@@ -36,7 +56,7 @@
             div.style.display = "inline-block";
             div.style.height = "100%";
             div.style.width = `${parseFloat(item.percent*100)}%`;
-            div.style.backgroundColor = item.color;
+            div.style.backgroundColor = (item.color) ? item.color : colorIt.next().value;
             div.style.position = "relative";
             div.classList.add('item-wrapper');
 
@@ -47,6 +67,7 @@
             span.style.position = "absolute";
             span.style.bottom = 8;
             span.style.right = 8;
+            span.style.fontSize = 12;
             span.classList.add('item-percentage');
 
             //Title span
@@ -90,16 +111,24 @@
     function getPercentages(data) {
         let sum = getSum(data);
 
-        return data.map((item) => {
+        return data.map(function (item) {
             return parseFloat(item.value / sum);
         });
     }
 
     //Accepts an array of chart data, returns the sum of all values
     function getSum(data) {
-        return data.reduce((sum, item) => {
+        return data.reduce(function (sum, item) {
             return sum + item.value;
         }, 0);
+    }
+
+    function* getNextColor() {
+        let i = 0;
+        while(true) {
+            yield palette[i];
+            i = (i + 1) % palette.length;
+        }
     }
 
 }(jQuery));
--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
 $('.chart').segbar([
     {
         data: [
-            { title: 'Portfolio 1', value: 16744, color: '#F22613' },
-            { title: 'Portfolio 2', value: 6500, color: '#9A12B3' },
-            { title: 'Portfolio 3', value: 32750, color: '#3FC380' },
-            { title: 'Portfolio 4', value: 3200, color: '#F9690E' }
+            { title: 'Portfolio 1', value: 16744 },
+            { title: 'Portfolio 2', value: 6500 },
+            { title: 'Portfolio 3', value: 32750 },
+            { title: 'Portfolio 4', value: 3200 }
+        ]
+    },
+    {
+        data: [
+            { title: 'Test 1', value: 6.87, color: '#8E44AD' },
+            { title: 'Test 2', value: 14.56, color: '#81CFE0' },
+            { title: 'Test 3', value: 7.82, color: '#EB974E' }
         ]
     }
 ]);
